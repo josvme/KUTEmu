c28a7904df1dc484ce86c3fab743f320a9d37e0b
Josvme <josvme@protonmail.com>
2025-02-09 16:44:03 +0100

    Hello World

--- C/OS/run.sh
+++ C/OS/run.sh
@@ -8,7 +8,7 @@ CC=clang
 CFLAGS="-fuse-ld=lld -std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c -Wl,--no-dynamic-linker
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c -Wl,--no-dynamic-linker
 
 # Start qemu
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
